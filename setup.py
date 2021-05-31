dc61ce69bfa2a43c3a2aaa576b546f6216516803
aholovenko <stacy@emedgene.com>
2021-05-31 22:29:05 +0300

    Add basic templates, refactoring

--- setup.py
+++ setup.py
@@ -1,56 +1,30 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Request
 from fastapi.responses import HTMLResponse
-from PIL import Image
-import logging
-import io
-import base64
-import uvicorn
+from fastapi.templating import Jinja2Templates
 import cv2
+import logging
+import uvicorn
 import numpy as np
+
+from logic import convert_to_base64, predict
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
-
-@app.get('/hello')
-async def say_hello():
-    return {"msg": "Hello radiologists!!"}
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def main():
-    content = """
-        <body>
-            <form action="/file/" enctype="multipart/form-data" method="post">
-                <input name="file" type="file" accept="image/png,image/jpg,image/jpeg">
-                <input type="submit">
-            </form>
-        </body>
-    """
-    return HTMLResponse(content=content)
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {
+        "title": "radiologists"
+    }
+    return templates.TemplateResponse("home.html", {"request": request, "data": data})
 
 
-def predict(img):
-    # TODO: add real pytorch model prediction
-    return [10, 10, 100, 100]
-
-
-def plot_bbox_onto_image(img, bbox):
-    x1, y1, x2, y2 = bbox
-
-    # Red color in BGR
-    color = (0, 0, 255)
-
-    # Line thickness of 2 px
-    thickness = 2
-
-    image_with_bbox = cv2.rectangle(img, (x1, y1), (x2, y2), color, thickness)
-    return image_with_bbox
-
-
-@app.post("/file/")
-async def create_file(file: bytes = File(...)):
+@app.post("/file", response_class=HTMLResponse)
+async def create_file(request: Request, file: bytes = File(...)):
     try:
         logging.info('Loading image...')
 
@@ -64,33 +38,18 @@ async def create_file(file: bytes = File(...)):
         logging.error(f'{msg}: {e}')
         return msg
 
-    # make prediction
-    bbox_resut = predict(opencvImage)
-    final_image = plot_bbox_onto_image(opencvImage, bbox_resut)
+    try:
+        output_image = predict(opencvImage)
+    except Exception as e:
+        msg = 'Error while prediction...'
+        logging.error(f'{msg}: {e}')
+        raise e
 
-    # convert CV2 to PIL
-    final_image = cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)
-    final_image = Image.fromarray(final_image)
-
-    # convert to file-like data
-    obj = io.BytesIO()  # file in memory to save image without using disk  #
-    final_image.save(obj, format='png')  # save in file (BytesIO)
-    obj.seek(0)
-
-    # convert to bases64
-    data = obj.read()  # get data from file (BytesIO)
-    data = base64.b64encode(data)  # convert to base64 as bytes
-    data = data.decode()  # convert bytes to string
-
-    # convert to <img> with embed image
-    content = f"""
-            <body>
-            <img style="width:400px;height:400px;" src="data:image/png;base64,{data}">
-            </body>
-            </div>
-        """
-
-    return HTMLResponse(content=content)
+    request_data = {
+        'input_image': convert_to_base64(file),
+        'output_image': convert_to_base64(output_image)
+    }
+    return templates.TemplateResponse("output.html", {"request": request, "data": request_data})
 
 
 if __name__ == '__main__':
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,13 +11,8 @@ class TestApplication(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.client = TestClient(app)
 
-    def test_read_main(self):
-        response = self.client.get("/hello")
-        assert response.status_code == 200
-        assert response.json() == {"msg": "Hello radiologists!!"}
-
     def test_file_upload(self):
         test_file_name = os.getcwd() + '/tests/test-ct-scan.jpg'
-        response = self.client.post('/file/', data={'file': open(test_file_name, 'rb')})
+        response = self.client.post('/file', data={'file': open(test_file_name, 'rb')})
         assert response.status_code == HTTPStatus.OK
         assert response.content != b'"Error while uploading. Please, make sure that you are uploading an image."'
