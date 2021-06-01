f15c400e55956d70ee15a7bfb78517c5ffe33921
Stacy <stacy@emedgene.com>
2021-06-01 17:36:19 +0300

    Add project desc to README.md (#10)

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,7 +12,6 @@ class TestApplication(unittest.TestCase):
         cls.client = TestClient(app)
 
     def test_file_upload(self):
-        test_file_name = os.getcwd() + '/tests/test-ct-scan.jpg'
-        response = self.client.post('/file', data={'file': open(test_file_name, 'rb')})
+        response = self.client.post('/file', data={'file': open('images/test-ct-scan.jpg', 'rb')})
         assert response.status_code == HTTPStatus.OK
         assert response.content != b'"Error while uploading. Please, make sure that you are uploading an image."'
